f0ced0042979034bb226c986f2b215b6aed9d012
alejandroroque-bit <alejandro.roque@3rcore.com>
2026-08-07 10:46:24 -0500

    Update Team.tsx

--- components/sections/Nosotros/Team.tsx
+++ components/sections/Nosotros/Team.tsx
@@ -41,7 +41,7 @@ const members = [
   { id: 21, name: "Jose", role: "Seo Manager", image: "/images/Equipo/jose.webp", area:"Web development & IT" },
   { id: 22, name: "Josue", role: "Web Developer", image: "/images/Equipo/josue-1.webp", area:"Web development & IT" },
   { id: 28, name: "Marxs", role: "Software Engineer", image: "/images/Equipo/Marxs.webp", area:"Web development & IT" },
-
+  { id: 29, name: "Bob", role: "AI - Software Engineer Agent", image: "/images/Equipo/bob.webp", area:"Web development & IT" },
 ];
 
 export default function Team() {
